152fc303f28c376a52ac4ee7687688a42d6b0ba9
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-10-07 11:16:51 -0400

    fix requirement.txt file

--- api/main.py
+++ api/main.py
@@ -30,7 +30,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://snooze-scribe.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
